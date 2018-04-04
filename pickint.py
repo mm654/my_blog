b5b9b3f426ebf52a00cd5bd2626830a3f456813d
mm654 <30494981+mm654@users.noreply.github.com>
2018-04-04 15:46:15 +0800

    Delete pickint.py

--- pickint.py
+++
@@ -1,9 +0,0 @@
-def pick_int(x):
-    if isinstance(x,float)==True:
-        integ=x-int(x)
-        if integ<0.5:
-            x=int(x)
-        else:
-            x=int(x)+1
-    return x
-print pick_int(4.5)
